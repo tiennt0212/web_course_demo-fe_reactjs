4955c61ce03452b322b03540133aef82d806bd53
tiennguyen <tiennguyen@lecle.co.kr>
2022-07-29 16:29:13 +0700

    using DM Mono font

--- src/App.js
+++ src/App.js
@@ -31,6 +31,33 @@ function App() {
         <div className='fw-800'>Space Grotesk 800</div>
         <div className='fw-900'>Space Grotesk 900</div>
 
+        <div className='lighter'>Space Grotesk (lighter/fw-300)</div>
+        <div className='normal'>Space Grotesk (normal/fw-400)</div>
+        <div className='bolder'>Space Grotesk (bold/bolder/fw-700)</div>
+
+
+        <div className='f-dm_mono lighter'>Font DM Mono with font-weight is Lighter</div>
+        <div className='f-dm_mono normal'>Font DM Mono with font-weight is Normal</div>
+        <div className='f-dm_mono bold'>Font DM Mono with font-weight is Bold</div>
+        <div className='f-dm_mono bolder'>Font DM Mono with font-weight is Bolder</div>
+        <div className='f-dm_mono fw-900'>Font DM Mono with font-weight is Bolder</div>
+
+        <div className='f-dm_mono fw-100'>Font DM Mono with font-weight is 100</div>
+        <div className='f-dm_mono fw-200'>Font DM Mono with font-weight is 200</div>
+        <div className='f-dm_mono fw-300'>Font DM Mono with font-weight is 300</div>
+        <div className='f-dm_mono fw-400'>Font DM Mono with font-weight is 400</div>
+        <div className='f-dm_mono fw-500'>Font DM Mono with font-weight is 500</div>
+        <div className='f-dm_mono fw-600'>Font DM Mono with font-weight is 600</div>
+        <div className='f-dm_mono fw-700'>Font DM Mono with font-weight is 700</div>
+        <div className='f-dm_mono fw-800'>Font DM Mono with font-weight is 800</div>
+        <div className='f-dm_mono fw-900'>Font DM Mono with font-weight is 900</div>
+
+        <div className='f-dm_mono lighter'>DM Mono (lighter)</div>
+        <div className='f-dm_mono normal'>DM Mono (normal)</div>
+        <div className='f-dm_mono fw-500'>DM Mono (fw-500)</div>
+        <div className='f-dm_mono bolder'>DM Mono (bold/bolder)</div>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
